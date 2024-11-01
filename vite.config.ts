d1312705561f417e416dae5b541a17ee0a266ca4
Bing <wangbing@hcjian.cn>
2024-11-01 20:07:18 +0800

    feat: mock接口数据模拟插件安装

--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,60 @@
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfigExport, ConfigEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { viteMockServe } from 'vite-plugin-mock';
+
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 //第一，引入path;node平台引入的模块，可以获得文件夹路径
 // @ts-ignore
 import path from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      symbolId: 'icon-[dir]-[name]',
-    }),
-  ],
-  //scss全局变量配置
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true,
-        additionalData: '@import "./src/styles/variable.scss";',
+
+export default ({ command }: ConfigEnv): UserConfigExport => {
+  return {
+    plugins: [
+      vue(),
+      viteMockServe({
+        mockPath: 'mock',
+        localEnabled: command === 'serve',//保证开发阶段可以使用mock接口
+        enable: true,
+      }),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        symbolId: 'icon-[dir]-[name]',
+      }),
+    ],
+    //scss全局变量配置
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@import "./src/styles/variable.scss";',
+        },
       },
     },
-  },
-  resolve: {
-    //第二，配置别名
-    alias: {
-      '@': path.resolve('./src'),
-    },
-  },
-  server: {
-    // 配置代理,可以配置多个地址
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      '/cci': {
-        target: 'ws://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/cci/, ''),
-      },
-      '/ws': {
-        target: 'ws://localhost:3000',
-        changeOrigin: true,
+    resolve: {
+      //第二，配置别名
+      alias: {
+        '@': path.resolve('./src'),
       },
     },
-  },
-});
+    server: {
+      // 配置代理,可以配置多个地址
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+        '/cci': {
+          target: 'ws://localhost:3000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/cci/, ''),
+        },
+        '/ws': {
+          target: 'ws://localhost:3000',
+          changeOrigin: true,
+        },
+      },
+    },
+  }
+};
