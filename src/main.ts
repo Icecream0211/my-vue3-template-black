2b71589c2480c0e2f56116365e137d7b46dc1b32
Bing <wangbing@hcjian.cn>
2024-11-01 19:02:55 +0800

    feat: 全局组件注册机制

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 //SVG插件需要配置的代码
 import 'virtual:svg-icons-register';
 import App from './App.vue';
-import gloalComponent from '@/components/index.ts'
+import gloalComponent from '@/components/index.ts';
 
 console.log(import.meta.env);
 console.log(import.meta.env.VITE_APP_BASE_API); // "123"
