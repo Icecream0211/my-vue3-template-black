0c5f28d4f1818ca132cedf75faa5d646ceb04e0e
Bing <wangbing@hcjian.cn>
2024-11-01 15:56:38 +0800

    feat: git提交规则校验

---
+++ commitlint.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    ignores: [(commit) => commit.includes('init')],
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test','pref', 'chore', 'revert','build']],
+        'type-case':[0],
+        'type-empty': [2, 'never'],
+        'scope-empty': [2, 'never'],
+        'scope-case': [0],
+        'subject-full-stop': [2, 'never'],
+
+        'body-leading-blank': [2, 'always'],
+        'footer-leading-blank': [1, 'always'],
+        'header-max-length': [2, 'always', 108],
+        'subject-empty': [2, 'never'],
+        'header-max-length': [0,'always',72],
+        'subject-case': [0,'never']
+    }
+}
