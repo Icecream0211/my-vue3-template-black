a40cc5251260280980e71d42dd72ebc23da2152d
Bing <wangbing@hcjian.cn>
2024-11-01 17:27:11 +0800

    feat: elementplus基础配置

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -2,10 +2,9 @@
  * pnpm 安装依赖的时候，会触发 preinstall钩子函数，使用到这个脚本，就可以做检查
  * */
 if (!/pnpm/.test(process.env.npm_execpath || '')) {
-    console.warn(
-      `\u001b[33mThis repository must using pnpm as the package manager ` +
-        ` for scripts to work properly.\u001b[39m\n`,
-    )
-    process.exit(1)
-  }
-  
+  console.warn(
+    `\u001b[33mThis repository must using pnpm as the package manager ` +
+      ` for scripts to work properly.\u001b[39m\n`,
+  );
+  process.exit(1);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-createApp(App).mount('#app');
+const app = createApp(App)
+app.use(ElementPlus,{
+    locale: zhCn,
+})
+app.mount('#app');
 
-const fun = () => {
-  console.log('未格式化代码');
-};
+//@ts-ignore  忽略ts校验
 
-fun();
