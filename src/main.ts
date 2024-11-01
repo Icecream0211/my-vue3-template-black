546d4e5147762bd5d5fedb29e8d998b485a3f9d4
Bing <wangbing@hcjian.cn>
2024-11-01 18:01:44 +0800

    feat: 环境变量配置;服务代理配置

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-
 console.log(import.meta.env);
-console.log(import.meta.env.VITE_APP_BASE_API) // "123"
-console.log(import.meta.env.VITE_SERVE) // undefined
-
+console.log(import.meta.env.VITE_APP_BASE_API); // "123"
+console.log(import.meta.env.VITE_SERVE); // undefined
 
 createApp(App).mount('#app');
